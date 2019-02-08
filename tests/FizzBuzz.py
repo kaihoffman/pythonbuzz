ef7adb6733405c56a59d3f852970189ac00394c3
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-02-08 10:18:02 +0000

    ddivisible by 5 test passes

--- tests/FizzBuzz.py
+++ tests/FizzBuzz.py
@@ -1,2 +1,5 @@
 def divisible_by_3(number):
     return number % 3 == 0
+
+def divisible_by_5(number):
+    return number % 5 == 0
--- tests/FizzbuzzTest.py
+++ tests/FizzbuzzTest.py
@@ -2,3 +2,6 @@ import FizzBuzz
 
 def test_divisible_by_3():
     assert FizzBuzz.divisible_by_3(3) == True
+
+def test_divisible_by_5():
+    assert FizzBuzz.divisible_by_5(5) == True
