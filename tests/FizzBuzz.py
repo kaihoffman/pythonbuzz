c05b8b0c77e9e61bc516878ee9265ab776c90b9a
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-02-08 10:34:28 +0000

    generic divisible_by method created

--- tests/FizzBuzz.py
+++ tests/FizzBuzz.py
@@ -3,3 +3,6 @@ def divisible_by_3(number):
 
 def divisible_by_5(number):
     return number % 5 == 0
+
+def divisible_by_number(number, divisor):
+    return number % divisor == 0
--- tests/FizzbuzzTest.py
+++ tests/FizzbuzzTest.py
@@ -5,3 +5,7 @@ def test_divisible_by_3():
 
 def test_divisible_by_5():
     assert FizzBuzz.divisible_by_5(5) == True
+
+def test_divisible_by_number():
+    assert FizzBuzz.divisible_by_number(5, 5) == True
+    assert FizzBuzz.divisible_by_number(3, 3) == True
